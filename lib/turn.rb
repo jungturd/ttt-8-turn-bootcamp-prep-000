c55d0fcc36915dd574a3bbb52dba406a9f611c00
Jeemin Shim <jeemin.shim@gmail.com>
2019-07-28 18:51:16 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,23 @@
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(choice)
+  choice.to_i - 1
+  #return new_user_input
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+  #return board
+end
+
+def turn(board)
+  puts "Please enter 1–9"
+end
+
+def 
